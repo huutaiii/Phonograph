dde76f8f6bda42ac2b0d7999f9d51c2f7d7516d1
huutai <truongtai137@gmail.com>
2021-02-11 11:51:06 +0700

    fix splash screen activity

--- app/src/main/java/com/kabouzeid/gramophone/ui/activities/LauncherActivity.java
+++ app/src/main/java/com/kabouzeid/gramophone/ui/activities/LauncherActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
